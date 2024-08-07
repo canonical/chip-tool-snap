97a9e4ba6525d745d4a26351c5e636d6016fccf1
JP Meijers <165929400+jpm-canonical@users.noreply.github.com>
2024-08-07 15:28:31 +0200

    Use matter-snap-testing to handle log files (#74)
    * Update log file path to directory
    * Update matter-snap-testing to beta.6
    * Remove duplication in log file names

--- tests/upgrade_test.go
+++ tests/upgrade_test.go
@@ -7,16 +7,18 @@ import (
 
 	"github.com/canonical/matter-snap-testing/env"
 	"github.com/canonical/matter-snap-testing/utils"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUpgrade(t *testing.T) {
 	start := time.Now()
 
 	t.Cleanup(func() {
+		// Remove snaps, ignoring errors during removal
 		utils.SnapRemove(nil, allClustersSnap)
-		utils.SnapDumpLogs(nil, start, allClustersSnap)
-
 		utils.SnapRemove(nil, chipToolSnap)
+
+		utils.SnapDumpLogs(t, start, allClustersSnap)
 	})
 
 	// Start clean
@@ -46,7 +48,7 @@ func TestUpgrade(t *testing.T) {
 
 	t.Run("Commission", func(t *testing.T) {
 		stdout, _, _ := utils.Exec(t, "chip-tool pairing onnetwork 110 20202021 2>&1")
-		writeLogFile(t, "chip-tool-pairing", []byte(stdout))
+		assert.NoError(t, utils.WriteLogFile(t, chipToolSnap, stdout))
 	})
 
 	t.Run("Control before upgrade", func(t *testing.T) {
@@ -56,7 +58,7 @@ func TestUpgrade(t *testing.T) {
 
 		start := time.Now()
 		stdout, _, _ := utils.Exec(t, "chip-tool onoff on 110 1 2>&1")
-		writeLogFile(t, "chip-tool-onoff", []byte(stdout))
+		assert.NoError(t, utils.WriteLogFile(t, chipToolSnap, stdout))
 
 		waitForOnOffHandlingByAllClustersApp(t, start)
 	})
@@ -69,14 +71,14 @@ func TestUpgrade(t *testing.T) {
 		}
 	})
 
-	t.Run("Control upgraded snap", func(t *testing.T) {
+	t.Run("Control after upgrade", func(t *testing.T) {
 		snapVersion := utils.SnapVersion(t, chipToolSnap)
 		snapRevision := utils.SnapRevision(t, chipToolSnap)
 		log.Printf("%s installed version %s build %s\n", chipToolSnap, snapVersion, snapRevision)
 
 		start := time.Now()
 		stdout, _, _ := utils.Exec(t, "chip-tool onoff off 110 1 2>&1")
-		writeLogFile(t, "chip-tool-onoff", []byte(stdout))
+		assert.NoError(t, utils.WriteLogFile(t, chipToolSnap, stdout))
 
 		waitForOnOffHandlingByAllClustersApp(t, start)
 	})
